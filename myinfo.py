234c56c7823ebb1334abb70daa0b4f856d76040c
shubhamjangid532 <shubhamjangid532@gmail.com>
2020-08-19 13:37:20 +0530

    Add RISE ID

--- myinfo.py
+++ myinfo.py
@@ -4,9 +4,10 @@ def MyDetails():
         print("""
 
                         \t Name : Shubham Jangid
+                        \t RISE ID : RISE.2020.28.6.4
                         \t Course : #IIEC_RISE #Specialist in Python
                         \t Mobile Number : 9649543318
-                        \t linkedin : https://www.linkedin.com/in/shubham-jangid-0959931a3/
-                        \t Email : shubhamjangid532@gmail.com
+                        \t linkedin ID : https://www.linkedin.com/in/shubham-jangid-0959931a3/
+                        \t Email ID : shubhamjangid532@gmail.com
         """)
         os.system("tput setaf 7")
