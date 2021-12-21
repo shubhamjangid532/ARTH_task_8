4915592af214b532bc5ba2f3a0b3327ef457c687
Shubham Jangid <53791298+shubhamjangid532@users.noreply.github.com>
2021-12-21 18:29:42 +0530

    Update myinfo.py

--- myinfo.py
+++ myinfo.py
@@ -4,12 +4,8 @@ def MyDetails():
         print("""
 
                         \t Name : Shubham Jangid
-                        \t      : Kumar Rahul
-                        \t      : Akhil Maharana
-                        \t      : Kanika Sharma
-                        \t      : Neeraj Pandey
-                        \t ARTH GROUP : ARTH2020_16_19
-                        \t GitHub Repo : https://github.com/shubhamjangid532/ARTH_task_8
+                        \t Roll no.: 20CMSXX634
+                        
 Post URL : https://www.linkedin.com/posts/shubham-jangid-0959931a3_create-a-menu-using-python-integrating-all-activity-6731270274647502848-M4VF
  Blog URL : https://shubhamjangid532.medium.com/create-a-menu-using-python-integrating-all-the-concepts-6d245604d547
         """)
